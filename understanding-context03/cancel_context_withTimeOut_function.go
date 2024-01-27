915e6af46e0608a5ff7741092641f9082e84c5a7
Kwe <Kweayon@gmail.com>
2024-01-26 19:12:01 -0600

    feat: working with context example 3

--- understanding-context03/cancel_context_withTimeOut_function.go
+++ understanding-context03/cancel_context_withTimeOut_function.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-// TODO: review code and comments
-
 func main() {
 	// instatiate parent context
 	ctx := context.Background()
@@ -20,27 +18,19 @@ func doSomething(ctx context.Context) {
 	// specify context timeout
 	var timeOut time.Duration = 3 * time.Second
 
-	// create a new context.Context scoped locally to the doSomething
-	// and a cancelation function returned by context.WithDeadline
-	// context.WithDeadline takes two arguments: parent context and a time.Duration object representing the time till cancelation
 	ctx, cancelContext := context.WithTimeout(ctx, timeOut)
 
-	// cancel context if timeout has not been exceeded
 	defer cancelContext()
 
-	// create out channel of integers to pass to worker goroutine
 	var out chan int = make(chan int)
 
 	go doAnother(ctx, out)
 
 	for n := 0; n < 5; n++ {
 		select {
-		// write the nth integer to out channel
 		case out <- n:
-			// wait for 1 second between out channel writes
 			time.Sleep(1 * time.Second)
 
-		// if the context is done stop attempting to write values to out channel and break out of the loop continuing function execution
 		case <-ctx.Done():
 			break
 		}
@@ -54,16 +44,13 @@ func doSomething(ctx context.Context) {
 func doAnother(ctx context.Context, in <-chan int) {
 	for {
 		select {
-		// read from in channel
 		case receivedChannelValue := <-in:
-			// process in channel value
 			fmt.Println("value recieved from in channel:", receivedChannelValue)
 
 		case <-ctx.Done():
 			var err error
 
 			if err = ctx.Err(); err != nil {
-				// TODO: send custom error depending on the error recieved | maybe see what the error string contains
 				fmt.Println("error from context:", err.Error())
 			}
 
